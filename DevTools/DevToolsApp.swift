78046fd409ee491ee890f95241a76f9eaefa7992
ibretsam <khanhle.3109@gmail.com>
2025-06-09 10:45:08 +0700

    Initial Commit

---
+++ DevTools/DevToolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevToolsApp.swift
+//  DevTools
+//
+//  Created by Khanh Le on 9/6/25.
+//
+
+import SwiftUI
+
+@main
+struct DevToolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DevToolsTests/DevToolsTests.swift
@@ -0,0 +1,16 @@
+//
+//  DevToolsTests.swift
+//  DevToolsTests
+//
+//  Created by Khanh Le on 9/6/25.
+//
+
+import Testing
+
+struct DevToolsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
